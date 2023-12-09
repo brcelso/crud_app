213863be00922610bdeb3f7d83b79fef5ce0a272
brcelso <129978598+brcelso@users.noreply.github.com>
2023-12-09 21:46:35 +0000

    ok

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -4,7 +4,7 @@ export default function Table(){
     return (
         <table className="min-w-full table auto rounded-md">
         <thead>
-            <tr className="bg-gray-700">
+            <tr className="bg-gray-900">
                 <th className="px-16 py-2">
                 <span className="text-gray-200">Event</span>
                 </th>
